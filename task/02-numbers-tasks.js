f4bce4c67d71fdfb432c6690ea80349053be53a9
Katya Ruzieva <katsiarynaruziyeva@gmail.com>
2021-04-07 15:15:22 +0300

    расстояние между точками

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -52,7 +52,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  throw new Error("Not implemented");
+  return (+value1 + +value2) / 2;
 }
 
 /**
@@ -71,7 +71,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  throw new Error("Not implemented");
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 /**
