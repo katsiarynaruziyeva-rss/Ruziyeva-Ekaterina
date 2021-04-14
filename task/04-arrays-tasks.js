884801ba51ebd12638b69bed0e399f6197b78a83
Katya Ruzieva <katsiarynaruziyeva@gmail.com>
2021-04-14 12:55:20 +0300

    get3TopItems

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -308,7 +308,10 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
 function get3TopItems(arr) {
-  throw new Error("Not implemented");
+  var arr2 = arr.sort(function (a, b) {
+    return b - a;
+  });
+  return arr2.slice(0, 3);
 }
 
 /**
