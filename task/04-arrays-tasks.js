c66a01d9dfd52b11d2d8d934794d25fc54e2b2ea
Katya Ruzieva <katsiarynaruziyeva@gmail.com>
2021-04-25 01:35:20 +0300

    sortCitiesArray

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -474,7 +474,19 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  */
 function sortCitiesArray(arr) {
-  throw new Error("Not implemented");
+  return arr.sort((a, b) => {
+    if (a.country < b.country) {
+      return -1;
+    } else if (a.country === b.country) {
+      if (a.city < b.city) {
+        return -1;
+      } else {
+        return 1;
+      }
+    } else {
+      return 1;
+    }
+  });
 }
 
 /**
